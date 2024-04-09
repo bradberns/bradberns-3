304cf877735771759e6a5e9a899cf48476f02964
bradbernsxxx <bradbernssd@gmail.com>
2024-04-09 11:52:29 -0700

    4-9-2024 Style clean-up 2

--- components/Cookie.vue
+++ components/Cookie.vue
@@ -7,16 +7,16 @@ const isOpen = ref(true)
   <div>
     <UModal class="cookies-banner text-center text-white md:text-black"
       v-model="isOpen" prevent-close>
-      <div class="p-4 md:text-sm">
+      <div class="p-4 md:text-md sm:text-sm">
         By clicking ”Accept”, you agree that 
         <span class=" underline font-semibold text-red-500"> 
         you are over the age of 21
       </span> and to the storing of cookies on your device to
       enhance site navigation and analitics.
         <div class="mt-2 text-center">
-          <UButton class="mr-2 rounded-md text-white" size="md"
+          <UButton class="mr-2 rounded-md text-white" size="lg"
               label="Accept" color="green" @click="isOpen = false" />
-          <UButton class="rounded-md text-white" size="md" 
+          <UButton class="rounded-md text-white" size="lg" 
           label="Decline" color="red" to="http://google.com" />
         </div>
       </div>
