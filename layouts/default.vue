0102468809a86a8fd988b0dc912c5712730dbe47
bradbernsxxx <bradbernssd@gmail.com>
2024-06-11 16:02:02 -0700

    6-11-2024 Improvments

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,10 @@
 <template>
   <div>
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <<!-- link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <Cookie id="cookie-banner"/>
+     --><Cookie id="cookie-banner"/>
     <Header />
     
       <slot />
