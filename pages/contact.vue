412caf22ab3f1b0853b81c34031c12774fbe7c50
bradbernsxxx <bradbernssd@gmail.com>
2024-11-08 16:17:19 -0800

    contact page update - 2

--- pages/contact.vue
+++ pages/contact.vue
@@ -106,16 +106,16 @@ useHead({
           icon="i-heroicons-pencil-square"
           label="Contact Me"/> -->
           <form @submit.prevent="submitForm">
-            <input class="mb-2 h-8 w-1/4 px-2 rounded" type="text" name="name" v-model="form.name"/>
+            <input class="mt-3 mb-2 h-8 w-1/2 sm:w-1/3 px-2 rounded bg-white" type="text" name="name" v-model="form.name" placeholder="name" />
             <br>
-            <input class="mb-2 h-8 w-1/4 px-2 rounded" type="email" name="email"  v-model="form.email"/>
+            <input class="mb-2 h-8 w-1/2 sm:w-1/3 px-2 rounded bg-white" type="email" name="email"  v-model="form.email" placeholder="email"/>
             <br>
-            <textarea class="mb-2 h-12 w-1/4 px-2 rounded" name="message" v-model="form.message"></textarea>
+            <textarea class="mb-2 h-12 w-1/2 sm:w-1/3 px-2 rounded bg-white" name="message" v-model="form.message" placeholder="message"></textarea>
             <br>
             <button class=" hover:bg-orange-500 hover:text-white 
-              bg-white w-1/5 rounded" type="submit">Send Message</button>
+              bg-white w-1/4 rounded" type="submit">Send</button>
           </form>
-      </div>
+      </div> 
     </div>
     <GetVids />
   </main>
